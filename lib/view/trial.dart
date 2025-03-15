c62af13e0676df7e138e511f618b4a4e83d492d4
mohamed hisham <mohamedhis36@gmail.com>
2025-03-15 17:18:30 +0200

    separated the trail assets list int its own file

---
+++ lib/view/trial.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:insgram_clone/view/core/constants/assets.dart';
+
+List<Widget> assetsTrial = const [
+  Image(
+    image: AssetImage(Assets.noProfile),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial3),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial1),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial2),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial1),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.noProfile),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial3),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial1),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial2),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial1),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.noProfile),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial3),
+    fit: BoxFit.cover,
+  ),
+  Image(
+    image: AssetImage(Assets.trial1),
+    fit: BoxFit.cover,
+  ),
+];
