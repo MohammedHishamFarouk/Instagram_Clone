db9ba078b152a5d1a9edbb92dcff05e8d06290bc
mohamed hisham <mohamedhis36@gmail.com>
2025-03-18 08:11:00 +0200

    added supabase key and changed the file name

--- lib/view/core/constants/cloud_service_keys.dart
+++ lib/view/core/constants/cloud_service_keys.dart
@@ -7,3 +7,8 @@ class FireKeys {
   static const String isEnglish = 'isEnglish';
   static const String userNameNormalized = 'NormalizedUserName';
 }
+
+class SupabaseKeys {
+  static const String anonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvenlzbmhnbGZwaGlkcnFtamt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNzQ3ODIsImV4cCI6MjA1Nzg1MDc4Mn0.2zdTeGpKA9zeR9nR9_wm4LXB8a9hYf3etdZagSu9Tos';
+}
